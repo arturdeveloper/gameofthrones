e5cebdbfea3b2a7bfb3067189d0d4a1307539d35
arturdeveloper <sato.alta@gmail.com>
2017-06-13 14:24:51 -0400

    removed styled compon

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
 
-export default styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-around;
-width: auto;
-height: 400px;
-border: 2px solid black;
-border-radius: 10px;
-align-items: center
-`;
+const ScrollItem = ()=> (
+        <div className="ScrollItem"></div>
+    );
+
+const CarouselItem = ()=> (
+    <div className="CarouselItem"></div>
+);
+
+export default ()=> {
+    return(
+        <div className="Carousel">
+            <ScrollItem />
+            <CarouselItem/>
+            <CarouselItem/>
+            <CarouselItem/>
+            <ScrollItem />
+        </div>
+    )
+}
--- src/components/CarouselItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-export default styled.div`
-width: 200px;
-height: 350px;
-border: 2px solid black;
-border-radius: 10px;
-`;
