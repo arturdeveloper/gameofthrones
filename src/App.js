2498f613bb1a445cdf9c7524d1f3ae97860f2fd2
Shabin <d.shabin@gmail.com>
2017-06-13 19:15:32 +0400

    Add fetch function

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ROOT_URL = 'http://api.tvmaze.com/singlesearch/shows'
+
+const div_flex_style = {
+  display : 'flex',
+  flex : 1 ,
+  justifyContent : 'center',
+  textAlign : 'center'
+}
+
+
 class App extends Component {
+
+  state = { episodes : ''}
+
+  async getData () {
+    let request ;
+    let query = '?q=game-of-thrones&embed=episodes'
+    try {
+      request = await fetch(ROOT_URL+ query)
+      //const request_status = await request.status
+      const request_json = await request.json()
+      this.setState({episodes : request_json['_embedded']['episodes']})
+      console.log(this.state.episodes)
+    }catch (e){
+      alert(e.message)
+    }
+
+
+  }
+
+
+
+  componentDidMount (){
+    this.getData()
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" style = {div_flex_style}>
+
       </div>
     );
   }
