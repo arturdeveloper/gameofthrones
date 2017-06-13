19afe1d634ad3c424753c4bcaa2ac70cb4882254
arturdeveloper <sato.alta@gmail.com>
2017-06-13 15:05:58 -0400

    added handlers

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const ScrollItem = ()=> (
-        <div className="ScrollItem"></div>
-    );
+const ScrollItem=styled.div`
+width: 5%;
+height: 10%;
+border: 2px solid black;
+border-radius: 10px;
+align-self: across-start;
+margin: 5px;
+`;
 
 const CarouselItem = ()=> (
     <div className="CarouselItem"></div>
 );
 
-export default ()=> {
+export default ({nextClickHandler:nxt, backClickHandler:bck})=> {
     return(
         <div className="Carousel">
-            <ScrollItem />
+            <ScrollItem onClick={bck} />
             <CarouselItem/>
             <CarouselItem/>
             <CarouselItem/>
-            <ScrollItem />
+            <ScrollItem onClick={nxt} />
         </div>
     )
 }
