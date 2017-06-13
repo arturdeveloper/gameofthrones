db56a25f031ab0da93aa26bbf8294ced6e9eb0f6
arturdeveloper <sato.alta@gmail.com>
2017-06-13 11:17:24 -0400

    some components

---
+++ src/components/Carousel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default styled.h2`
+justifgy-content: center;
+width: 800px;
+height: 400px;
+border: 2px solid black;
+border-radius: 10px;
+`;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default styled.h2`
+justifgy-content: center;
+width: auto;
+border: 2px solid palevioletred;
+border-radius: 3px;
+`;
